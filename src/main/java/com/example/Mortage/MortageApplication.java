c9aecb0863a389f2a4d0381bbc079ea79a8ef8bf
rksiet2 <rksiet2@gmail.com>
2019-07-12 15:19:50 +0530

    Building project

---
+++ src/main/java/com/example/Mortage/MortageApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mortage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortageApplication.class, args);
+	}
+
+}
